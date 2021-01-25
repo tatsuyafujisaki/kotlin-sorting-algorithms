917c237ec207a2f0a0a56b084769bf83adf1eaad
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-25 23:58:08 +0900

    Create CoutingSort.kt

---
+++ CoutingSort.kt
@@ -0,0 +1,14 @@
+object CountingSort {
+    fun sort(xs: IntArray) {
+        val occurrences = IntArray(xs.maxOrNull()!! + 1)
+        for (x in xs) {
+            occurrences[x]++
+        }
+        var i = 0
+        occurrences.forEachIndexed { j, x ->
+            repeat(x) {
+                xs[i++] = j
+            }
+        }
+    }
+}
