a26817df38afb412edbadc8af64278eb6d9c4a89
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-31 14:56:37 +0900

    Update QuicksortRecursive.kt

--- QuicksortRecursive.kt
+++ QuicksortRecursive.kt
@@ -1,8 +1,6 @@
 object QuicksortRecursive {
     fun sort(xs: IntArray): IntArray {
-        if (xs.isEmpty()) {
-            return xs
-        }
+        if (xs.isEmpty()) return xs
         val pivot = xs[xs.size / 2]
         return sort(xs.filter { it < pivot }.toIntArray()) +
             xs.filter { it == pivot } +
